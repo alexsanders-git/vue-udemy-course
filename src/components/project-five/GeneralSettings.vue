f62837b9c2099096337961c685f5ba18cb89e75a
alex <alex.sanders@ukr.net>
2025-03-15 20:27:26 +0200

    Project Five: Create General Settings Form

--- src/components/project-five/GeneralSettings.vue
+++ src/components/project-five/GeneralSettings.vue
@@ -1,7 +1,62 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppButton from '@/components/AppButton.vue';
+</script>
 
 <template>
-  <div>General</div>
-</template>
+  <div>
+    <h2 class="mb-4 text-2xl">General</h2>
+    <form class="space-y-4 mx-auto">
+      <div class="space-y-2">
+        <label>Username</label>
+        <input type="text" />
+      </div>
 
-<style scoped></style>
+      <div class="space-y-2">
+        <label>Email</label>
+        <input type="email" />
+      </div>
+
+      <div class="space-y-2">
+        <label>About Me</label>
+        <textarea></textarea>
+      </div>
+
+      <div class="space-y-2">
+        <label>Gender</label>
+
+        <label>
+          <input type="radio" value="male" />
+          <span>Male</span>
+        </label>
+
+        <label>
+          <input type="radio" value="female" />
+          <span>Female</span>
+        </label>
+
+        <label>
+          <input type="radio" value="other" />
+          <span>Other</span>
+        </label>
+      </div>
+
+      <div class="space-y-2">
+        <label>Country</label>
+        <select>
+          <option>USA</option>
+          <option>United Kingdom</option>
+          <option>Canada</option>
+          <option>Australia</option>
+          <option>Germany</option>
+          <option>France</option>
+          <option>Italy</option>
+          <option>Spain</option>
+          <option>Japan</option>
+          <option>India</option>
+        </select>
+      </div>
+
+      <AppButton>Save</AppButton>
+    </form>
+  </div>
+</template>
